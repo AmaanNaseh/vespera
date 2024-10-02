30bab427f8bb7b8bea2e948871717f8cd327a484
Chaitanya Gupta <guptadivya958@gmail.com>
2024-10-02 20:04:02 +0530

    added event page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import "./App.css";
 import image from "./image/full.png"
 import Header from "./components/header.jsx";
+import Event from "./components/event.jsx";
 function App() {
   const myStyle = {
     backgroundImage: `url(${image})`,
     backgroundSize: "cover",
     backgroundRepeat: "no-repeat",
-    height: "450vh"
+    height: "750vh"
     
   };
   return <div style={myStyle}>
-    <Header/>
+    <Event />
   </div>
 }
 
